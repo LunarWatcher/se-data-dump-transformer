5b9495eccbba0e961a74d03b520509646f41346f
Olivia <zoe.i2k1@gmail.com>
2024-08-02 20:13:10 +0200

    Minor cleanup

--- transformer/cmake/FindPackageHandleStandardArgs.cmake
+++ transformer/cmake/FindPackageHandleStandardArgs.cmake
@@ -1,9 +1,10 @@
 # I hate this so fucking much
 function(FIND_PACKAGE_HANDLE_STANDARD_ARGS _NAME _FIRST_ARG)
     if ("${_NAME}" STREQUAL "ZSTD")
-        message(INFO "Hijacking FIND_PACKAGE_HANDLE_STANDARD_ARGS for ZSTD")
+        message(STATUS "Hijacking FIND_PACKAGE_HANDLE_STANDARD_ARGS for ZSTD")
         find_package(${_NAME} REQUIRED)
     else()
+        message(STATUS "Forwarding ${_NAME} to the built-in find_package_handle_standard_args")
         set (OLD_CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}")
         set (CMAKE_MODULE_PATH "")
 
