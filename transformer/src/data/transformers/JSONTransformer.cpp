78b4799e85e989a463962bd3c5067dcd65f0a280
Olivia <zoe.i2k1@gmail.com>
2024-08-16 21:24:46 +0200

    Replace yyjson with nlohmann/json
    This is _not_ optimal _at all_. nlohmann/json is fantastic, but it isn't
    designed for anywhere near this much data.
    Unfortunately, yyjson seems to have trouble with the output from the XML
    parser, while nlohmann/json doesn't.
    Even with UTF-8 validation disabled, yyjson died on non-breaking spaces
    and emojis, and I do not understand why.
    When preparing a demo for a bug report, I was unable to reproduce with
    the _exact same input strings_.
    I imagine there's something weird with the interaction between the two,
    but I'm unable to determine what, so I can't open a bug report.
    
    I don't blame yyjson, for the record; this was not a problem with the
    previous data dump, so something new in SE's 2024-06-30 data dump is
    completely fucked, and only nlohmann/json seems able to deal with it.
    No amount of string sanitising seems to be able to deal with it, and I
    don't understand why.
    
    I'm not sure if it really matters anyway. Yes, significantly worse
    throughput from nlohmann/json, but the vast majority of the time
    consumed does not go to JSON generation; it goes to zipping up tens of
    gigabytes of data.

--- transformer/src/data/transformers/JSONTransformer.cpp
+++ transformer/src/data/transformers/JSONTransformer.cpp
@@ -3,9 +3,10 @@
 #include "data/Schema.hpp"
 #include "spdlog/spdlog.h"
 #include "util/StringSanitiser.hpp"
-#include "wrappers/yyjson.hpp"
 #include <sstream>
 #include <stdexcept>
+
+#include <nlohmann/json.hpp>
 
 namespace sedd {
 
@@ -35,65 +36,59 @@ void JSONTransformer::endArchive(const ParserContext&) {
 }
 
 void JSONTransformer::parseLine(const pugi::xml_node& row, const ParserContext& ctx) {
-    YYJsonWriter jw;
-    yyjson_mut_val* obj = yyjson_mut_obj(*jw);
-    if (obj == nullptr) {
-        throw std::runtime_error("Failed to allocate JSON object");
-    }
-    yyjson_mut_doc_set_root(*jw, obj);
-
     const auto& types = Schema::schema.at(ctx.currType);
 
-    // This read-only container is used to contain the strings from date and string types.
-    // yyjson_mut_obj_add_str does not copy the string, so this is required to keep the strings valid
-    std::vector<std::string> stringLifecycleContainer;
+    // Using nlohmann/json was not my first choice, and is not optimal here
+    // Given the large amount of data, performance is incredibly important. 
+    // Unfortunately:
+    // * yyjson struggles with utf8, and fails to convert the 2024-06-30 data dump
+    //   which came with unicode encoding errors.
+    // * rapidjson has not released a new version since 2016, and has multiple vulnerabilities
+    // * simdjson (currently) doesn't seem to support serialisation, only reading,
+    //   which is useless here.
+    //
+    // Until yyjson starts working or simdjson is fixed, I guess we're doing a suboptimal
+    // solution
+    
+    nlohmann::json root = nlohmann::json::object();
 
     for (const auto& attr : row.attributes()) {
         // TODO: check if the second condition is necessary or not
         if (attr.empty() || attr.value() == nullptr) {
-            yyjson_mut_obj_add_null(*jw, obj, attr.name());
+            //yyjson_mut_obj_add_null(*jw, obj, attr.name());
+            root[attr.name()] = nullptr;
             continue;
         }
 
         switch (types.at(attr.name()).type) {
         case Schema::LONG:
-            yyjson_mut_obj_add_int(*jw, obj, attr.name(), attr.as_llong());
+            //yyjson_mut_obj_add_int(*jw, obj, attr.name(), attr.as_llong());
+            root[attr.name()] = attr.as_llong();
             break;
         case Schema::DOUBLE:
-            yyjson_mut_obj_add_real(*jw, obj, attr.name(), attr.as_double());
+            //yyjson_mut_obj_add_real(*jw, obj, attr.name(), attr.as_double());
+            root[attr.name()] = attr.as_double();
             break;
         case Schema::BOOL:
-            yyjson_mut_obj_add_bool(*jw, obj, attr.name(), attr.as_bool());
+            //yyjson_mut_obj_add_bool(*jw, obj, attr.name(), attr.as_bool());
+            root[attr.name()] = attr.as_bool();
             break;
         case Schema::STRING:
         case Schema::DATE:
-            stringLifecycleContainer.emplace_back(
-                StringSanitiser::cleanString(attr.as_string())
-            );
-            yyjson_mut_obj_add_str(*jw, obj, attr.name(), stringLifecycleContainer.back().c_str());
+            //yyjson_mut_obj_add_str(*jw, obj, attr.name(), stringLifecycleContainer.back().c_str());
+            root[attr.name()] = StringSanitiser::cleanString(attr.as_string());
             break;
-        default:
+        [[unlikely]] default:
             throw std::runtime_error("Invalid type for field " + std::string(attr.name()));
         }
     }
 
-    auto json = jw.write();
-    if (json.error()) {
-        std::stringstream ss;
-        row.print(ss);
-
-        spdlog::error("JSON write error: {}. attr value: {}", json.err.msg, ss.str());
-        for (auto& string : stringLifecycleContainer) {
-            spdlog::debug("String: {}", string);
-        }
-        throw std::runtime_error("Failed to construct JSON string");
-    }
     if (this->started) {
         this->writer->write(",\n");
     } else {
         this->started = true;
     }
-    this->writer->write(json.str);
+    this->writer->write(root.dump());
 }
 
 }
