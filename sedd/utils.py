ec8de6edbbdc84ff6fe75bd047b31dd4f467c0c5
Oleg Valter <o.a.philippov@gmail.com>
2024-08-24 06:32:39 +0300

    added file-related utils

--- sedd/utils.py
+++ sedd/utils.py
@@ -2,6 +2,8 @@ from typing import Dict
 import requests as r
 from urllib.parse import urlparse
 import os.path
+import re
+
 
 def extract_etag(url: str, etags: Dict[str, str]):
     res = r.get(
@@ -21,3 +23,39 @@ def extract_etag(url: str, etags: Dict[str, str]):
     etags[filename] = etag
 
     print(f"ETag for {filename}: {etag}")
+
+
+def get_file_name(site_or_url: str):
+    file_name = f"{re.sub(r'https://', '', site_or_url)}.7z"
+
+    file_name = re.sub(r'^alcohol', 'beer', file_name)
+    file_name = re.sub(r'^mattermodeling', 'materials', file_name)
+    file_name = re.sub(r'^communitybuilding', 'moderators', file_name)
+    file_name = re.sub(r'^medicalsciences', 'health', file_name)
+    file_name = re.sub(r'^psychology', 'cogsci', file_name)
+    file_name = re.sub(r'^writing', 'writers', file_name)
+    file_name = re.sub(r'^video', 'avp', file_name)
+    file_name = re.sub(r'^meta\.(es|ja|pt|ru)\.', r'\1.meta.', file_name)
+
+    return file_name
+
+
+def check_file(base_path: str, file_name: str):
+    try:
+        res = os.stat(os.path.join(base_path, file_name))
+        return res.st_size > 0
+    except FileNotFoundError:
+        return False
+
+
+def remove_old_file(base_path: str, site_or_url: str):
+    try:
+        file_name = get_file_name(site_or_url)
+        os.remove(os.path.join(base_path, file_name))
+    except FileNotFoundError:
+        pass
+
+
+def is_file_downloaded(base_path: str, site_or_url: str):
+    file_name = get_file_name(site_or_url)
+    return check_file(base_path, file_name)
